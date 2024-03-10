bab09019cdc47b0bb64ba0ae46035a3e28402f10
Charles Souza <charles.souza.silva1995@gmail.com>
2024-03-10 15:23:06 -0300

    Foi realizado um CRUD e pequenos testes usando o swagger e postman

--- CadastroDeUsuario/Controllers/UsuarioController.cs
+++ CadastroDeUsuario/Controllers/UsuarioController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CadastroDeUsuario.Data;
+using CadastroDeUsuario.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CadastroDeUsuario.Controllers
 {
     public class UsuarioController : ControllerBase
     {
+        private UsuarioContext _context;
 
+        public UsuarioController(UsuarioContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaUsuario([FromBody] Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return Ok(usuario);
+        }
     }
 }
